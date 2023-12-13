35e02f68a703e11d35326236f5aaef9594ccb7af
Abhay Swarnakar <68654016+GENRATECODE@users.noreply.github.com>
2023-12-13 14:34:51 +0530

    add

--- Sorting and Searching/concert_tickets.py
+++ Sorting and Searching/concert_tickets.py
@@ -3,4 +3,9 @@ h=list(map(int,input().split()))
 t=list(map(int,input().split()))
 h.sort()
 t.sort()
- 
+for i in t:
+    for j in h:
+        if i<=j:
+            print(i)
+            h.pop(i)
+        e
